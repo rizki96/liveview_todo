43340ae0fd986887fed2c7adb78d14e87ca63a6d
Iskandar Rizki <iskandar.rizki@bhinneka.com>
2020-07-31 20:20:37 +0700

    add handle_event for add_todo

--- lib/liveview_todo_web/live/todo_live.ex
+++ lib/liveview_todo_web/live/todo_live.ex
@@ -2,11 +2,19 @@ defmodule LiveviewTodoWeb.TodoLive do
     use LiveviewTodoWeb, :live_view
   
     alias LiveviewTodoWeb.Todos.AddTodoFormComponent
+    require Logger
 
     @impl true
     def mount(_params, _session, socket) do
         
         {:ok, assign(socket, todos: %{})}
+    end
+
+    @impl true
+    def handle_event("add_todo", %{"todo" => todo} = _params, socket) do
+        Logger.log(:debug, "#{inspect todo}")
+
+        {:noreply, socket}
     end
 
 end
